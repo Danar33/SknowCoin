79f9af3be09848d44569cde3f6a7ee544e68ddf1
Oscar <oscardaniel199518@gmail.com>
2017-04-27 19:42:51 -0500

    Objetos necesarios para firebase

--- app/src/main/java/dan_art/sknowcoin/MainActivity.java
+++ app/src/main/java/dan_art/sknowcoin/MainActivity.java
@@ -14,7 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
+//Main
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
