c33ea4ab8b603b53d2fd6ccac21304a537dbc559
Oscar <oscardaniel199518@gmail.com>
2017-05-04 20:17:04 -0500

    Autenticacion

---
+++ app/src/main/java/dan_art/sknowcoin/Firebase/Autenticacion.java
@@ -0,0 +1,62 @@
+package dan_art.sknowcoin.Firebase;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by VAIO PRO on 04/05/2017.
+ */
+
+public class Autenticacion {
+
+    private FirebaseAuth mAuth;
+
+    public Autenticacion() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signUp(String email, String password, final Context contexto) {
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(contexto, "Bienvenido.", Toast.LENGTH_SHORT).show();
+
+                    //TODO: Ir a la actividad que corresponde
+
+                } else {
+                    Toast.makeText(contexto, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void signIn(String email, String password, final Context contexto){
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Toast.makeText(contexto, "Bienvenido.", Toast.LENGTH_SHORT).show();
+
+                    //TODO: Ir a la actividad que corresponde
+
+                } else {
+                    Toast.makeText(contexto, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public FirebaseAuth getmAuth() {
+        return mAuth;
+    }
+}
